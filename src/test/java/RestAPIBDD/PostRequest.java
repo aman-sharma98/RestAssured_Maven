37ea04afce4735992fc52963a67fb125e3c05361
aman-sharma98 <sharmaaman1998@hotmail.com>
2021-12-26 13:41:17 +0530

    RestAssured Maven Commit

---
+++ src/test/java/RestAPIBDD/PostRequest.java
@@ -0,0 +1,35 @@
+package RestAPIBDD;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class PostRequest {
+	
+	@Test
+	public void PostCall() {
+		
+		Map<String,Object> PostBody = new HashMap<String, Object>();
+		PostBody.put("name", "Ama");
+		PostBody.put("salary", "5000");
+		
+		RestAssured.given()
+        .baseUri("http://localhost:7000")
+        .contentType(ContentType.JSON)
+        .accept(ContentType.JSON)
+        .body(PostBody)
+        .when()
+        .post("/employees/create")
+        .then()
+        .statusCode(201)
+        .log()
+        .all();
+		
+	}
+
+}
---
+++ src/test/java/restAPI/GetRequest.java
@@ -0,0 +1,25 @@
+package restAPI;
+
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class GetRequest {
+	
+	@Test
+	public void GetCall() {
+		
+		RestAssured.baseURI = "http://localhost:7000";
+		RequestSpecification request = RestAssured.given();
+		Response response = request.get("/employees");
+		String ResponseBody = response.body().asString();
+		System.out.println(ResponseBody);
+		
+	}
+
+}
